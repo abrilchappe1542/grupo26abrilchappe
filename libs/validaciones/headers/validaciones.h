03b9bfe571c0516e806b7cda38e7e502b4ccb21f
abrilchappe <aechappe.cnsc@gmail.com>
2026-04-03 19:35:51 -0300

    Subiendo TP completo con librerías

---
+++ libs/validaciones/headers/validaciones.h
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+
+#define VALIDACIONES_H
+
+
+/* >>>>> VALIDACIONES DE STRINGS DE NUMEROS <<<<< */
+
+bool esDigito(const char * cadena); // Retorna TRUE si el string representa solo números enteros.
+
+bool esDigito_positivo(const char * cadena); // Retorna TRUE si el string representa solo números enteros positivos.
+
+bool noEsCero(const char * cadena); // Retorna TRUE si el string representa un numero distinto de cero.
+
+bool esFlotante(const char *c);
+
+bool esFlotantePositivo(const char *c);
+
+
+/* >>>>> VALIDACIONES DE STRINGS <<<<< */
+
+bool esAlfabetico(const char *c); // Retorna TRUE si el string representa solo valores alfabeticos.
+
+bool soloEspacios(char* ); // Retorna TRUE si el string contiene al menos un caracter.
+
+bool masDeUnCaracter(char*); // Retorna TRUE si el string contiene mas de a un caracter.
+
+bool contieneEspacios(const char *);    //Retorna TRUE si el string NO contiene espacios.
+
+bool soloHyL(char senial[]);    //si la señal solo contiene h o l retorna verdadero.
+
+bool verificandoSenial(char senial[]);  //si la señal solo contiene h o l retorna verdadero.
+
+
+/* >>>>> INGRESOS <<<<< */
+
+char* ingresa_palabra(char* );  // Facilita el ingreso de un string. 
+
+bool ingresoDatosNumericosEnter(const char *mensaje_error, int *num);
+
+int ingresoDatosNumericos(char * mensaje_error); // Facilita el ingreso y la validacion de valores numericos.
+
+int ingresoDatosNumericosPositivos(char * mensaje_error); // Facilita el ingreso y la validacion de valores numericos positivos >= 0.
+
+int ingresoDatosNumericosNoCero(char * mensaje_error); // Facilita el ingreso y la validacion de valores numericos positivos > 0.
+
+int ingresoIntLimitado(char * mensaje_error, int minimo, int maximo); // Ingreso de valores 'int' estableciento un rango.
+
+int ingresoIntMinimo(char * mensaje_error, int minimo); // Ingreso de valores 'int' estableciento un minimo (incluido).
+
+double ingresoDatosFloat(char *mensaje_error); // Facilita el ingreso y la validacion de valores float.
+
+double ingresoDatosFloatPositivos(char *mensaje_error); // Facilita el ingreso y la validacion de valores float positivos >= 0.
+
+
+/* >>>>> HERRAMIENTAS <<<<< */
+
+char* sacarEspacios(char* ); // Elimina los espacios de un string.
+
+void todoAMayusculas(char* string); // Convierte un string a mayusculas.
+
+int preguntarContinuar(); // Pregunta si se desea continuar
+
+int leerEnteroPositivo(const char* mensaje);
+
+void reemplazarComasPorPuntos(char *c);
+
+void mostrarComparacion(int respuesta); 
+
