b1b930f22abcefcd2e50ed23eca0a7fbd96e74e0
JuanPP3-18 <127800528+JuanPP3-18@users.noreply.github.com>
2026-04-04 16:28:55 -0300

    cambio en el menupunto1

--- menu.c
+++ menu.c
@@ -2,34 +2,31 @@
 
 
 void menuPunto1(){
-    char* palabra = NULL;
-    int seguir = 1;
+        int seguir = 1;
+    char palabra[100];
+    char palabralimpia[100];
 
     while(seguir == 1){
         bool bandera = false;
-        while (!bandera)    //hasta que la palabra ingresada no sea valida, no sale del while
-        {   
-            palabra = ingresa_palabra("Ingrese un palindromo: \n");
-            bandera = (esAlfabetico(palabra) && soloEspacios(palabra) && masDeUnCaracter(palabra) && contieneEspacios(palabra));
-            if(!bandera){   //si no es valido el ingreso del string, libera la memoria utilizada
-                printf("\nIngrese una palabra valida! Que no contenga espacios, sea mas de un caracter y no sean numeros.\n");
-                free(palabra);
-                palabra = NULL;
-            }
-        }
-        
-        todoAMayusculas(palabra);
-        bandera = palindromo(palabra);  
+
+        printf("Ingrese una palabra o frase: ");
+        fgets(palabra, sizeof(palabra), stdin); 
+
+        palabra[strcspn(palabra, "\n")] = '\0';
+
+        ingresarpalindromo(palabra, palabralimpia);
+
+        // todoAMayusculas(palabra);
+        bandera = palindromo(palabralimpia);  
         
         if(bandera == true){
-            printf("La palabra %s, es un palindromo\n",palabra);
+            printf("La palabra o frase %s, es un palindromo\n",palabra);
         }
 
         else if(bandera == false){
-            printf("La palabra %s, NO es un palindromo\n",palabra);
+            printf("La palabra o frase %s, NO es un palindromo\n",palabra);
         }
 
-        free(palabra);
         printf(".\n.\n.\n>>>> Desea ingresar otro valor?\n");
         seguir = preguntarContinuar();
         system("cls");
