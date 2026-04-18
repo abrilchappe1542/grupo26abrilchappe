33e8cd6a49ffb11f9b6395307587bc0278b1575e
salva616 <162522370+salva616@users.noreply.github.com>
2026-04-18 16:34:40 -0300

    Update menu.c

--- programacion2-2026/02-trabajoPractico-Listas/menu.c
+++ programacion2-2026/02-trabajoPractico-Listas/menu.c
@@ -31,3 +31,30 @@ void menuPunto3(){
         system("cls");
     }
 }
+
+//menu punto 4:
+void menuPunto4(){
+    Lista list1, list2;
+    int cantidad, seguir = 1;
+
+    while(seguir == 1){
+        printf("Ingrese la cantidad de elementos a cargar en las listas\n-> ");
+        cantidad = ingresoDatosNumericosNoCero("Ingrese un numero valido!");
+
+        printf("\nCargamos lista 1:\n");
+        list1 = l_cargar(cantidad);
+        printf("\nCargamos lista 2:\n");
+        list2 = l_cargar(cantidad);
+
+        printf("\nLISTA 1: \n");
+        l_mostrar(list1);
+        printf("\nLISTA 2: \n");
+        l_mostrar(list2);
+
+        printf("|\n|\n|__RESULTADO DE LA COMPARACION:\n");
+        CompararListas(list1, list2);
+
+        seguir = preguntarContinuar();
+        system("cls");
+    }
+}
