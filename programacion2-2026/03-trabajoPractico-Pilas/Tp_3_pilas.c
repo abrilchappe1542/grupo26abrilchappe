4b72f2dc673258ce9759c30a7eb6cd8bd9641862
JuanPP3-18 <127800528+JuanPP3-18@users.noreply.github.com>
2026-05-11 18:56:12 -0300

    subo punto 6

---
+++ programacion2-2026/03-trabajoPractico-Pilas/Tp_3_pilas.c
@@ -0,0 +1,61 @@
+#include "..\libs\pilas\headers\pilas.h"
+#include "..\libs\tipoElemento\headers\tipo_elemento.h"
+#include "..\libs\validaciones\headers\validaciones.h"
+#include "tp_3_pilas.h"
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+//punto 6
+Pila p_ej6_eliminarclave(Pila p, int clave){
+    Pila paux = p_crear();
+    Pila pauxres = p_crear();
+    Pila paux2 = p_crear();
+    TipoElemento elem;
+    while (!p_es_vacia(p))
+    {   
+        elem = p_desapilar(p);
+        if (elem->clave != clave)
+        {
+            p_apilar(paux,elem);
+        }
+        p_apilar(paux2,elem);       //esto es para no perder la original
+    }
+    
+    while (!p_es_vacia(paux2))
+    {
+        elem = p_desapilar(paux2);
+        p_apilar(p,elem);
+    }
+    while (!p_es_vacia(paux))
+    {
+        elem = p_desapilar(paux);
+        p_apilar(pauxres,elem);         //aca hago la lista para retornar en el mismo orden
+    }
+    return pauxres;
+}
+
+//---------------------------------------------------DE FORMA RECURSIVA----------------------------------
+
+void p_ej6_eliminarclaveRecursivoAux(Pila p, int clave){
+    if (p_es_vacia(p))
+    {
+        return;
+    }
+    TipoElemento elem = p_desapilar(p);
+    p_ej6_eliminarclaveRecursivoAux(p,clave);
+
+    if (elem->clave != clave)
+    {
+        p_apilar(p,elem);
+    }
+    p_apilar(p,elem); //reconstruyo la lista;
+}
+
+Pila p_ej6_eliminarclaveRecursivo(Pila p, int clave){
+    p_ej6_eliminarclaveRecursivoAux(p,clave);
+    return p;
+}
