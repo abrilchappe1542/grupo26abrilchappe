8ccbfb3a1b6cd6980571ab3a38329581c81500b4
LHMulka <Lhmlk.4@gmail.com>
2026-05-18 17:02:04 -0300

    Update menu.h

--- programacion2-2026/03-trabajoPractico-Pilas/menu.h
+++ programacion2-2026/03-trabajoPractico-Pilas/menu.h
@@ -3,7 +3,7 @@
 #define MENU_H
 //void menuPunto2();
 //void menuPunto3();
-//void menuPunto4();
+void menuPunto4();
 void menuPunto5();
 void menuPunto6();
 Pila p_ej6_eliminarclaveRecursivo(Pila p, int clave);
