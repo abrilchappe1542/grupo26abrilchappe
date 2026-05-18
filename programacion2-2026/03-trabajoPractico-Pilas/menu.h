9396318fe305c35321d658ef45b97c337f9a06c9
salva616 <162522370+salva616@users.noreply.github.com>
2026-05-17 22:47:36 -0300

    ejercicio5 descomentado

--- programacion2-2026/03-trabajoPractico-Pilas/menu.h
+++ programacion2-2026/03-trabajoPractico-Pilas/menu.h
@@ -4,7 +4,7 @@
 //void menuPunto2();
 //void menuPunto3();
 //void menuPunto4();
-//void menuPunto5();
+void menuPunto5();
 void menuPunto6();
 Pila p_ej6_eliminarclaveRecursivo(Pila p, int clave);
 void menuPunto7();
