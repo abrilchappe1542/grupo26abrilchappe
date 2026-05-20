05de764759a7bcaf557bc7ba4495cf47578dd1c9
JuanPP3-18 <127800528+JuanPP3-18@users.noreply.github.com>
2026-05-20 17:32:58 -0300

    Update menu.h

--- programacion2-2026/03-trabajoPractico-Pilas/menu.h
+++ programacion2-2026/03-trabajoPractico-Pilas/menu.h
@@ -6,7 +6,6 @@ void menuPunto3();
 void menuPunto4();
 void menuPunto5();
 void menuPunto6();
-Pila p_ej6_eliminarclaveRecursivo(Pila p, int clave);
 void menuPunto7();
 void menuPunto8();
 #endif // MENU_H
