3ebc2559ea2819aa13eb913ab8c5033290c47dd8
JuanPP3-18 <127800528+JuanPP3-18@users.noreply.github.com>
2026-06-09 19:19:22 -0300

    subo punto 2

--- programacion2-2026/05-trabajoPractico-Arboles/tp_5_arboles.c
+++ programacion2-2026/05-trabajoPractico-Arboles/tp_5_arboles.c
@@ -1,6 +1,103 @@
 #include "tp_arboles.h"
 
 
+//EJERCICIO 2
+//Punto A
+void nodoHojasAux(NodoArbol nodo, Lista lres){
+    if (nodo == NULL)
+    {
+        return;
+    }
+    
+    else
+    {
+        if (n_hijoizquierdo(nodo) == NULL && n_hijoderecho(nodo) == NULL)
+        {
+            l_agregar(lres,n_recuperar(nodo));
+        }
+        nodoHojasAux(n_hijoizquierdo(nodo),lres);
+        nodoHojasAux(n_hijoderecho(nodo),lres);
+    }   
+}
+
+Lista a_ej2_hojas(ArbolBinario a){
+    Lista lres = l_crear();
+    if (!a_es_vacio(a))
+    {
+        NodoArbol nodo = a_raiz(a);
+        nodoHojasAux(nodo,lres);
+    }
+    return lres;
+}
+
+//!-------------------------------------------------------------------------------------------------------------------------------------
+//                                                       PUNTO B
+//!-------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+void NodosInternosAux(NodoArbol nodo, Lista lres){
+    if (nodo == NULL)
+    {
+        return;
+    }
+    else
+    {
+        if (n_hijoizquierdo(nodo) != NULL || n_hijoderecho(nodo) != NULL)
+        {
+            l_agregar(lres,n_recuperar(nodo));
+        }
+
+        NodosInternosAux(n_hijoizquierdo(nodo),lres);
+        NodosInternosAux(n_hijoderecho(nodo),lres);
+    }
+}
+
+Lista a_ej2_interiores(ArbolBinario a){
+    Lista lres = l_crear();
+    if (!a_es_vacio(a))
+    {
+        NodoArbol raiz = a_raiz(a);
+        NodosInternosAux(n_hijoizquierdo(raiz),lres);
+        NodosInternosAux(n_hijoderecho(raiz),lres);
+    }
+    return lres;   
+}
+
+//!-------------------------------------------------------------------------------------------------------------------------------------
+//                                                       PUNTO C
+//!-------------------------------------------------------------------------------------------------------------------------------------
+
+void punto2CAux(NodoArbol nodo, Lista lres, int clave){
+    if (nodo == NULL)
+    {
+        return;
+    }
+    else
+    {
+        if (n_recuperar(nodo)->clave == clave)
+        {
+            l_agregar(lres, te_crear_con_valor(0,nodo));
+        }
+        punto2CAux(n_hijoizquierdo(nodo),lres,clave);
+        punto2CAux(n_hijoderecho(nodo),lres,clave);
+    }
+    
+}
+
+Lista a_ej2_buscarclave(ArbolBinario A, int clave){
+    Lista lres = l_crear();
+    if (a_es_vacio(A))
+    {
+        return lres;
+    }
+    NodoArbol nodo = a_raiz(A);
+    punto2CAux(nodo,lres,clave);
+    return lres;
+}
+
+
+
 // EJERCICIO 7
 bool determinarEquivalencia(NodoArbol nodo1, NodoArbol nodo2){
 
