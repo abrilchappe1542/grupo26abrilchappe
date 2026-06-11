0ba47081d5f2f2d1bac68ff64a2e8b3240b1214a
alesin24 <alexisnavarro1618@gmail.com>
2026-06-11 09:46:06 -0300

    punto 9

--- programacion2-2026/05-trabajoPractico-Arboles/tp_5_arboles.c
+++ programacion2-2026/05-trabajoPractico-Arboles/tp_5_arboles.c
@@ -1,5 +1,6 @@
 #include "tp_arboles.h"
-
+#include <stdio.h>
+#include <time.h>
 
 // EJERCICIO 7
 bool determinarEquivalencia(NodoArbol nodo1, NodoArbol nodo2){
@@ -40,4 +41,59 @@ bool a_ej7_equivalente(ArbolBinario A, ArbolBinario B){
     equivalencia = determinarEquivalencia(raiz1, raiz2);
 
     return equivalencia;
+}
+
+//EJERCICIO 9
+Lista a_ej10_generarlistaclaves(int cantidadclavesagenerar, int valorminimo, int valormaximo) {
+    Lista l = l_crear(); 
+    int generados = 0;
+
+    while (generados < cantidadclavesagenerar) {
+        int numero_azar = (rand() % (valormaximo - valorminimo + 1)) + valorminimo;
+        if (l_buscar(l, numero_azar) == NULL) { 
+            TipoElemento te = te_crear(numero_azar); 
+            l_agregar(l, te); 
+            generados++;
+        }
+    }
+    return l;
+}
+
+ArbolBinarioBusqueda a_ej10_crearABB(Lista L) {
+    ArbolBinarioBusqueda abb = abb_crear();
+    Iterador iter = iterador(L);
+
+    while (hay_siguiente(iter)) {
+        TipoElemento te = siguiente(iter);
+        abb_insertar(abb, te);
+    }
+    return abb;
+}
+
+ArbolAVL a_ej10_crearAVL(Lista L) {
+    ArbolAVL avl = avl_crear();
+    Iterador iter = iterador(L);
+
+    while (hay_siguiente(iter)) {
+        TipoElemento te = siguiente(iter);
+        avl_insertar(avl, te);
+    }
+    return avl;
+}
+
+Lista a_ej10_comparacionarboles(int N_repeticiones, int valorminimo, int valormaximo, int cantidaclavesagenerar) {
+    Lista resultados_diferencias = l_crear();
+
+    for (int i = 0; i < N_repeticiones; i++) {
+        Lista claves = a_ej10_generarlistaclaves(cantidaclavesagenerar, valorminimo, valormaximo);
+
+        ArbolBinarioBusqueda abb = a_ej10_crearABB(claves);
+        ArbolAVL avl = a_ej10_crearAVL(claves);
+
+        int diferencia = a_ej10_difalturas(abb, avl);
+
+        TipoElemento te_dif = te_crear(diferencia);
+        l_agregar(resultados_diferencias, te_dif);
+    }
+    return resultados_diferencias;
 }
